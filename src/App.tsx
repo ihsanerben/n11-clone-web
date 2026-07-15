e657e9e7eae1c217dda5e3b9115cd0ae3ccfafd9
ihsanerben <ihsanerben@gmail.com>
2026-07-15 13:04:32 +0300

    chore: initialize React Vite project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        n11 Clone Web
+      </h1>
+    </main>
+  )
+}
+
+export default App
